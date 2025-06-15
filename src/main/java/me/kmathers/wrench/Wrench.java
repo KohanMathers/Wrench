c2b87774e6143fb4d8e6387dd75f2d7e9f6e2c24
Kohan Mathers <kmathers@kmathers.co.uk>
2025-06-16 00:26:49 +0100

    Throw InterruptedException

--- src/main/java/me/kmathers/wrench/Wrench.java
+++ src/main/java/me/kmathers/wrench/Wrench.java
@@ -3,7 +3,7 @@ package me.kmathers.wrench;
 import me.kmathers.wrench.network.TcpServer;
 
 public class Wrench {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello, Wrench!");
         int port = 25565;
         TcpServer server = new TcpServer(port);
