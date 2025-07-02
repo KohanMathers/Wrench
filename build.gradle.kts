a5b633cf5ca75bc9430b4fb628901a825e5f6c02
Kohan Mathers <kmathers@kmathers.co.uk>
2025-07-02 23:16:04 +0100

    Initialise the server (it's alive!)

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("java")
+    id("com.gradleup.shadow") version "8.3.0"
+}
+
+group = "org.wrench"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.minestom:minestom-snapshots:4fe2993057")
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+tasks {
+    jar {
+        manifest {
+            attributes["Main-Class"] = "org.wrench.Main"
+        }
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+    shadowJar {
+        mergeServiceFiles()
+        archiveClassifier.set("")
+    }
+}
