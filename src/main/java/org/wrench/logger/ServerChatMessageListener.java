94bff7ebb54fd94bdf2676456ed6e8da80dfbbe7
KohanMathers <kmathers@kmathers.co.uk>
2025-07-03 20:52:29 +0100

    Add player death logging

--- src/main/java/org/wrench/logger/ServerChatMessageListener.java
+++ src/main/java/org/wrench/logger/ServerChatMessageListener.java
@@ -1,7 +1,9 @@
 package org.wrench.logger;
 
+import net.kyori.adventure.text.Component;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.PlayerChatEvent;
+import net.minestom.server.event.player.PlayerDeathEvent;
 
 public final class ServerChatMessageListener {
 
@@ -10,6 +12,12 @@ public final class ServerChatMessageListener {
             String message = event.getRawMessage();
             String playerName = event.getPlayer().getUsername();
             org.wrench.Main.log("info", "<" + playerName + "> " + message);
+        });
+
+        handler.addListener(PlayerDeathEvent.class, event -> {
+            String playerName = event.getPlayer().getUsername();
+            Component deathMessage = event.getDeathText();
+            org.wrench.Main.log("info", playerName + deathMessage);
         });
     }
 }
